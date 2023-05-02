b9c1c36b181ab68039a1dffd631049a8cd97bf9c
Brett Park <brett@shiverware.com>
2023-05-02 13:07:03 -0600

    20.0.2 Beta

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BlinkUp",
-            url: "https://github.com/electricimp/BlinkUpSDK-iOS/releases/download/20.0.1/blinkup_sdk_ios_v20.0.1.xcframework.zip",
-            checksum: "768f0ab8c090222c2733ecaeeea354f27bcd512e6dfc97df97932374140e18f6"),
+            url: "https://github.com/electricimp/BlinkUpSDK-iOS/releases/download/20.0.2/blinkup_sdk_ios_v20.0.2.xcframework.zip",
+            checksum: "f7b0d66e0c5c340ca9f58968536ce7893e3d3f4cd2ccfa31aff431b87f3a6f46"),
     ]
 )
 
