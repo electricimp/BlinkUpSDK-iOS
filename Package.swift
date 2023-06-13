50be0229953570035289dccd88a84632a9e5ea20
Brett Park <brett@shiverware.com>
2023-06-12 22:36:28 -0600

    Update to newer beta checksum

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .binaryTarget(
             name: "BlinkUp",
             url: "https://github.com/electricimp/BlinkUpSDK-iOS/releases/download/v20.1.0/blinkup_sdk_ios_v20.1.0.xcframework.zip",
-            checksum: "c8bb77e4f7837f24860e5c109eb6de22a4be8c334df91b18c850d7173f60ca55"),
+            checksum: "3638e573a6377e1ce341e4b93c200f749e15021c9598f7ce320a1ef761f75ab9"),
     ]
 )
 
